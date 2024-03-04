913564c431be0ab7900845f6eb59349c4bcd518c
pros2018 <prosenjitbari2018@gmail.com>
2024-03-04 23:52:19 +0530

    my app code

---
+++ target/dist/mywebapp-1.0.dev0/mywebapp.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+# Create a Flask application
+app = Flask(__name__)
+
+# Define a route for the root URL
+@app.route('/')
+def hello():
+    return 'Hello, welcome to my simple web application!'
+
+# Run the application
+if __name__ == '__main__':
+    app.run(debug=True,port='7001')
